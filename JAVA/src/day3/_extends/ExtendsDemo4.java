535729f821816eb2868b2c92e7e14fc35919d378
ccc212 <2120496201@qq.com>
2023-11-01 10:35:13 +0800

    day3

---
+++ JAVA/src/day3/_extends/ExtendsDemo4.java
@@ -0,0 +1,29 @@
+package day3._extends;
+
+public class ExtendsDemo4 {
+    public static void main(String[] args) {
+        C c = new C();
+        c.methodC();
+        c.methodB();
+        c.methodA();
+    }
+}
+
+//java支持单继承，不支持多继承
+class A {
+    public void methodA() {
+        System.out.println("A...");
+    }
+}
+
+class B extends A {
+    public void methodB() {
+        System.out.println("B...");
+    }
+}
+
+class C extends B {
+    public void methodC() {
+        System.out.println("C...");
+    }
+}
---
+++ JAVA/src/day3/_static/Student.java
@@ -0,0 +1,7 @@
+package day3._static;
+
+public class Student {
+    String name;
+    int age;
+    static String school;
+}
