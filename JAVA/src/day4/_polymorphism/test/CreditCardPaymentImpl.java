d223249e7848b02b9e368f27b3d878751f72f3ea
ccc212 <2120496201@qq.com>
2023-11-04 14:15:02 +0800

    temp

---
+++ JAVA/src/day4/_polymorphism/test/CreditCardPaymentImpl.java
@@ -0,0 +1,8 @@
+package day4._polymorphism.test;
+
+public class CreditCardPaymentImpl implements Payment {
+    @Override
+    public void pay(double money) {
+        System.out.println("通过信用卡快捷支付:" + money + "元!");
+    }
+}
---
+++ JAVA/src/day4/_polymorphism/test/Payment.java
@@ -0,0 +1,5 @@
+package day4._polymorphism.test;
+
+public interface Payment {
+    void pay(double money);
+}
