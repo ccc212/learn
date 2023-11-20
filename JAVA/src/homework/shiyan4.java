94b3d43c73b69ae4b2ed8c957dd809de9257934c
ccc212 <2120496201@qq.com>
2023-11-20 15:55:18 +0800

    update

--- JAVA/src/homework/shiyan4.java
+++ JAVA/src/homework/shiyan4.java
@@ -1,4 +1,82 @@
 package homework;
 
-public class shiyan4 {
+import java.util.ArrayList;
+import java.util.List;
+
+//编写一个实体类(如学生、用户、老师等)，该泛型类具有插入实体对象和输出List内容的方法。
+//     要求1. 该实体类至少有一个编号的成员变量和设置编号的成员方法。在设置编号时，要求进行编号校验
+//     （自定义校验规则：如编号的前4位为2019等等），符合规则则返回true,否则返回false。
+//        2.定义存放实体类的List。
+//        3.能够实现对该List增加和删除一个实体对象，并显示操作后的结果。
+interface Entity{
+    String getId();
+    String getName();
 }
+class Stu implements Entity{
+
+    private String id;
+    private String name;
+    public Stu() {
+    }
+    public Stu(String id,String name){
+        if("2019".equals(id.substring(0,4))){
+            this.id = id;
+        }
+        this.name = name;
+    }
+    public boolean setId(String id) {
+        if("2019".equals(id.substring(0,4))){
+            this.id = id;
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+}
+class MyList<T extends Entity>{
+    private List<T> MyList;
+    public MyList(){
+        MyList = new ArrayList<>();
+    }
+    public void printEntity(){
+        System.out.println("当前成员有:");
+        for (T entity : MyList) {
+            System.out.println("id:"+entity.getId()+"\t"+"name:"+entity.getName());
+        }
+        System.out.println();
+    }
+    public void addEntity(T entity){
+        System.out.println("添加成员"+entity.getName());
+        MyList.add(entity);
+        printEntity();
+    }
+    public void moveEntity(T entity){
+        System.out.println("删除成员"+entity.getName());
+        MyList.remove(entity);
+        printEntity();
+    }
+}
+public class shiyan4 {
+    public static void main(String[] args) {
+        MyList<Stu> stuList=new MyList<>();
+        Stu stu1=new Stu();
+        Stu stu2=new Stu();
+        System.out.println(stu1.setId("2019001"));
+        stu1.setName("张三");
+        System.out.println(stu2.setId("2018002"));
+        stu2.setName("李四");
+        stuList.addEntity(stu1);
+        stuList.addEntity(stu2);
+        stuList.moveEntity(stu1);
+    }
+}
+
--- JAVA/src/homework/shiyan5.java
+++ JAVA/src/homework/shiyan5.java
@@ -1,4 +1,25 @@
 package homework;
 
+import javax.swing.*;
+
+//用图形界面来实现实验4的要求，点击“确认”或“删除”按钮时，增加或删除List中的一个对象。
+// 点击“显示”按钮时，显示List中的全部对象。
 public class shiyan5 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("MyList");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        JButton btn1 = new JButton("增加");
+        JButton btn2 = new JButton("删除");
+        JButton btn3 = new JButton("显示");
+        btn1.setBounds(10, 10, 80, 50);
+        btn2.setBounds(100, 10, 80, 50);
+        btn3.setBounds(190, 10, 80, 50);
+        frame.add(btn1);
+        frame.add(btn2);
+        frame.add(btn3);
+        frame.setVisible(true);
+    }
 }
