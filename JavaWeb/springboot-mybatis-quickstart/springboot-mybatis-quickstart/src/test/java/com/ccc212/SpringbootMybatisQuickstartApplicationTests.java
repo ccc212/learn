cd25e074cdc3db8adee2e3557c9f3c0a24d5fae3
ccc212 <2120496201@qq.com>
2023-12-13 22:07:17 +0800

    back

---
+++ JavaWeb/springboot-mybatis-quickstart/springboot-mybatis-quickstart/src/test/java/com/ccc212/SpringbootMybatisQuickstartApplicationTests.java
@@ -0,0 +1,23 @@
+package com.ccc212;
+
+import com.ccc212.mapper.UserMapper;
+import com.ccc212.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest//springboot整合单元测试的注解
+class SpringbootMybatisQuickstartApplicationTests {
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void testListUser(){
+        List<User> userList=userMapper.list();
+        userList.stream().forEach(user -> {
+            System.out.println(user);
+        });
+    }
+
+}
